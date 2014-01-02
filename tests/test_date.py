769f1b46e60def2675a14bd5872047af6d1ea398
sax <s.apostolico@gmail.com>
2014-01-02 17:38:00 +0100

    bump 0.5

--- tests/test_date.py
+++ tests/test_date.py
@@ -1,16 +1,18 @@
 import datetime
 from time import timezone
+import pytest
 from sample_data_utils.date import date, day
 from dateutil.relativedelta import relativedelta, SU
 
 today = datetime.date.today()
 
 tomorrow = today + relativedelta(days=+1)
+next_tomorrow = today + relativedelta(days=+2)
 next_month = today + relativedelta(months=+1)
 next_year = today + relativedelta(years=+1)
 
-def test_date():
 
+def test_date():
     c = date(today, tomorrow)
     assert c == today
 
@@ -23,5 +25,16 @@ def test_date():
 
 def test_day():
     c = day(today, next_month, [SU])
-    assert next_month > c > today
+    assert next_month >= c >= today
     assert c.weekday() == 6
+
+    c = day(today, tomorrow)
+    assert next_month >= c >= today
+    assert c in [today, tomorrow]
+
+    c = day(today, today)
+    assert c == today
+
+    with pytest.raises(IndexError):
+        c = day(today, tomorrow, [SU])
+
